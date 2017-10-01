5bda2be06289016aaba2d60ae0fb97ae443e8dda
Tsuyoshi Yasunishi <tsuyok@gmail.com>
2017-10-02 08:18:51 +0900

    OAuth+Googleを追加

--- src/main/kotlin/example/Application.kt
+++ src/main/kotlin/example/Application.kt
@@ -2,10 +2,33 @@ package example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @SpringBootApplication
-class Application
+@EnableOAuth2Sso
+class Application : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .antMatcher("/**")
+                .authorizeRequests().antMatchers("/", "/login**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .logout().logoutRequestMatcher(AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+    }
+}
 
 fun main(args: Array<String>) {
-	SpringApplication.run(Application::class.java, *args)
+    SpringApplication.run(Application::class.java, *args)
 }
